1ced2c8c240fbd458a4f7df4c504e72417ba2047
jinwooorp <jim8912woo@gmail.com>
2025-03-24 15:55:53 +0900

    0324

---
+++ hyp_app/android/app/src/main/kotlin/com/example/hyp_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hyp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hyp_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('앱'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Text('앱'),
+
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
